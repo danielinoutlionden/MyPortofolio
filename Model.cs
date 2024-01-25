afbe0a3bb759f5ae43a5c3c22b0c1a19324c2925
danielinoutlionden <157679741+danielinoutlionden@users.noreply.github.com>
2024-01-26 05:23:01 +0700

    Add files via upload

---
+++ Model.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace Backtowork
+{
+
+
+    public class SavedataContext : DbContext
+    {
+        public DbSet<mahasiswa> Students { get; set; }
+
+        public string DbPath { get; }
+
+        public SavedataContext()
+        { 
+            var path = Environment.CurrentDirectory;
+            DbPath = System.IO.Path.Join(path, "Savedata.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+    public class mahasiswa
+    {
+        public int Id { get; set; }
+        public string text { get; set; } 
+    }
+
+}
